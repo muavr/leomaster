6efbff409ed85603457fa3fc23152337c61b394e
Muravev Anton <muravevanton@gmail.com>
2017-10-29 20:41:36 +0300

    gh-1 Modifies encoding while reading file to utf-8 in load_from_file(...) method
    Also adds test for  load_from_file(...).
    Adds leo_page_content.html which contains test data for parsing.
    Fixes omitted self in get content method of LeoparserTest class.
    Renames LeoparserTest to LeoParserTest

--- leoparser/leoparser.py
+++ leoparser/leoparser.py
@@ -1,12 +1,15 @@
+# -*- coding: utf-8 -*-
+
 import lxml.html
 import requests
 
-class LeoParser():
+
+class LeoParser(object):
     def __init__(self):
         self._content = ''
 
     def load_from_file(self, path: str):
-        with open(path, 'r') as f_dsc:
+        with open(path, 'r', encoding='utf-8') as f_dsc:
             self._content = f_dsc.read()
             f_dsc.close()
 
--- test/test_leoparser.py
+++ test/test_leoparser.py
@@ -1,16 +1,17 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 from leoparser.leoparser import LeoParser
 
 
-class LeoparserTest(unittest.TestCase):
+class LeoParserTest(unittest.TestCase):
     TEST_DATA_PATH = './data/leo_page_content.html'
 
-    def  get_parser(self):
+    def get_parser(self):
         return LeoParser()
 
-    def  get_content(self):
-        content = ''
-        with open(TEST_DATA_PATH, 'r') as f_dsc:
+    def get_content(self):
+        with open(self.TEST_DATA_PATH, 'r', encoding='utf-8') as f_dsc:
             content = f_dsc.read()
             f_dsc.close()
         return content
@@ -20,6 +21,13 @@ class LeoparserTest(unittest.TestCase):
         object_id_in_hex = hex(id(lp))
         expected_value = '<LeoParser ({})>'.format(object_id_in_hex)
         self.assertEqual(lp.__str__(), expected_value)
+
+    def test_loading_from_file(self):
+        lp = self.get_parser()
+        lp.load_from_file(self.TEST_DATA_PATH)
+        expected_value = self.get_content()
+
+        self.assertEqual(lp._content, expected_value)
 
 if __name__ == '__main__':
     unittest.main()
