d853c89e0d27aa4460c3c639ccc79594fa0be640
muavr <muravevanton@gmail.com>
2018-06-03 12:07:22 +0300

    Removes tests for Masterclasses class.

--- test/test_masterclass.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import unittest
-from leoparser.mastercalss import Masterclass
-
-
-class LeoParserTest(unittest.TestCase):
-    def get_masterclass(self):
-        return Masterclass()
