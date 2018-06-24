8da5f2784fc233c64aec80939c97cbab27182f72
muavr <muravevanton@gmail.com>
2018-06-24 21:09:59 +0300

    gh-8 Implements simple bot that allows to send messages

--- leomaster_site/leomaster_app/settings_local.template.py
+++ leomaster_site/leomaster_app/settings_local.template.py
@@ -1,6 +1,9 @@
 import os
 from django.conf import settings
 
+LEO_TELEGRAM_MODE = 'HTML'
+LEO_TELEGRAM_BOT_TOKEN = '<token>'
+LEO_TELEGRAM_CHAT_ID = '<chat_id>'
 LEO_TASK_LOG_PATH = '../log/task.log'
 LEO_TASK_LOG_LEVEL = 'DEBUG'
 LEO_PARSER_CONFIG_PATH = os.path.join(settings.BASE_DIR, '../leoparser/parser_config.json')
