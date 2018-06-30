0edf66f5145041516124b9abd571952a41f835be
muavr <muravevanton@gmail.com>
2018-06-30 14:50:29 +0300

    gh-9 Adds Celery task for user notifications

--- leomaster_site/leomaster_app/tasks.py
+++ leomaster_site/leomaster_app/tasks.py
@@ -1,17 +1,23 @@
+import pytz
+import time
 import json
 import requests
-import fake_useragent
 import celery.signals
 
+from django.template import loader
+from django.utils import translation
+from django.utils.formats import date_format
 from celery.utils.log import get_task_logger
 from logging.handlers import TimedRotatingFileHandler
 
+from leobot.bot import LeoBot
 from leomaster_app.models import *
 from leomaster_app.settings import *
 from leomaster_site.celery import app
 from leoparser.leoparser import LeoParserFabric
 
 CONTENT_URL = 'https://leonardo.ru/masterclasses/petersburg/'
+CURRENT_TZ = 'Europe/Moscow'
 
 logger = get_task_logger(__name__)
 
@@ -29,7 +35,7 @@ def on_after_setup_task_logger(**kwargs):
         def_logger.addHandler(time_rot_fh)
 
 
-@app.task(bind=True)
+@app.task(bind=True, ignore_result=True)
 def update(self):
     logger.info('Starting update task')
     user_agent = fake_useragent.UserAgent()
@@ -78,7 +84,29 @@ def update(self):
         if not Masterclass.objects.filter(uid=key).exists():
             mc = Masterclass.objects.create(**body, master=master, location=location)
             logger.info('New masterclass was added: "{0}"'.format(mc.uid))
+            notify.delay(mc.id)
         else:
             logger.info('Masterclass already exists: "{0}"'.format(key))
 
     logger.info('Update task finished%s%s%s' % ('\n', '=' * 25, '\n'))
+
+
+@app.task(bind=True, max_retries=20, ignore_result=True, default_retry_delay=60*2.5)
+def notify(self, mc_id):
+    translation.activate('ru')
+    leobot = LeoBot(LEO_TELEGRAM_BOT_TOKEN)
+    mc = Masterclass.objects.get(pk=mc_id)
+    template = loader.get_template('leomaster_app/telegram_notification.html')
+    context = {'title': mc.title,
+               'when': date_format(mc.date.astimezone(pytz.timezone(mc.location.tz)), 'd-m-Y H:i, l'),
+               'where': mc.location.name,
+               'who': mc.master.name,
+               'howmany': mc.avail_seats,
+               'preview': mc.img_url,
+               'online_price': mc.online_price,
+               'price': mc.price,
+               'description': mc.description,
+               'target': CONTENT_URL}
+    rendered = template.render(context=context)
+    leobot.send_message(LEO_TELEGRAM_CHAT_ID, rendered)
+
