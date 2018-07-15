c9a12fa3bf20cce9b57325938ccee027db46ead4
muavr <muravevanton@gmail.com>
2018-07-15 23:46:59 +0300

    gh-14 Adds url

--- leomaster_site/leomaster_site/urls.py
+++ leomaster_site/leomaster_site/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+from leomaster_app import views
 
 urlpatterns = [
+    path('', views.show_last_added_masterclasses),
     path('admin/', admin.site.urls),
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
