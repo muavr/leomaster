62bb775d815e915d6f9ebb1699c90aa771667dff
muavr <muravevanton@gmail.com>
2018-08-05 19:14:24 +0300

    gh-33 Fix time localization error

--- leomaster_site/leomaster_app/views.py
+++ leomaster_site/leomaster_app/views.py
@@ -20,15 +20,16 @@ class MasterclassViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         group = self.kwargs.get('group', '')
+        tz = pytz.timezone(settings.TIME_ZONE)
         if group == 'topic':
             queryset = Masterclass.objects.all().filter(date__gte=timezone.now()).order_by('-date')
         elif group == 'half_month':
             half_month_ago = datetime.today() - timedelta(days=15)
-            half_month_ago = half_month_ago.astimezone(pytz.timezone(settings.TIME_ZONE))
+            half_month_ago = tz.localize(half_month_ago)
             queryset = Masterclass.objects.all().filter(creation_ts__gte=half_month_ago).order_by('-creation_ts')
         elif group == 'month':
             month_ago = datetime.now() - timedelta(days=31)
-            month_ago = month_ago.astimezone(pytz.timezone(settings.TIME_ZONE))
+            month_ago = tz.localize(month_ago)
             queryset = Masterclass.objects.all().filter(creation_ts__gte=month_ago).order_by('-creation_ts')
         elif group == 'all':
             queryset = Masterclass.objects.all().order_by('-date')
