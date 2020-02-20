fd32c98dcac7e06275ea16ca2662f322bfe2e74f
Anton Muravev <muravevanton@gmail.com>
2020-02-20 23:47:34 +0300

    #gh-44/Adds models for parser rules

--- leomaster_site/leoparser/models.py
+++ leomaster_site/leoparser/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Rule(models.Model):
+    name = models.TextField(verbose_name='name', unique=True, blank=False)
+    xpath = models.TextField(verbose_name='xpath', blank=False)
+    typeof = models.ForeignKey('TypeOf', on_delete=models.CASCADE)
+
+
+class TypeOf(models.Model):
+    name = models.TextField(verbose_name='name', unique=True, blank=False)
