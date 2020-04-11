dbbd2f3083ae5d80271f696c7fcf99d161193d5b
Anton Muravev <muravevanton@gmail.com>
2020-04-12 00:32:38 +0300

    signal: add signal for RemovableHistoryDocument
    Add signal that creates the delta of document automatically

--- leomaster_site/leoparser/signals.py
+++ leomaster_site/leoparser/signals.py
@@ -1,10 +1,17 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from leoparser.models import Document, DocDelta
+from leoparser.models import Document, DocDelta, RemovableHistoryDocument
 
 
 @receiver(post_save, sender=Document)
 def save_document_delta(sender, instance, **kwargs):
+    delta = list(instance.delta)
+    if delta:
+        DocDelta.objects.create(base=instance, delta=delta)
+
+
+@receiver(post_save, sender=RemovableHistoryDocument)
+def save_removable_history_document_delta(sender, instance, **kwargs):
     delta = list(instance.delta)
     if delta:
         DocDelta.objects.create(base=instance, delta=delta)
